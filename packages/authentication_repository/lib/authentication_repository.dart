177f4d3531cf2c78af63460b2b7b9526b772b611
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-07 14:24:57 +1000

    feat(authentication_repository,user_entity): hand-amend, add firestore model converter, nullsafety

--- packages/authentication_repository/lib/authentication_repository.dart
+++ packages/authentication_repository/lib/authentication_repository.dart
@@ -1,4 +1,5 @@
 library authentication_repository;
 
 export 'src/authentication_repository.dart' show AuthenticationRepository;
+export 'src/models/user_entity.dart' show UserEntity;
 export 'src/models/user_model.dart' show User;
--- packages/authentication_repository/lib/src/models/user_entity.dart
+++ packages/authentication_repository/lib/src/models/user_entity.dart
@@ -1,30 +1,25 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart'
+    show DocumentReference, DocumentSnapshot;
 
 class UserEntity {
-  final String emailAddress;
-  final String lastName;
-  final String firstName;
-  final DocumentSnapshot snapshot;
-  final DocumentReference reference;
-  final String userID;
-
-  UserEntity({
-    this.emailAddress,
-    this.lastName,
-    this.firstName,
-    this.snapshot,
-    this.reference,
-    this.userID,
+  const UserEntity({
+    required this.userID,
+    required this.emailAddress,
+    required this.lastName,
+    required this.firstName,
+    required this.snapshot,
+    required this.reference,
   });
 
   factory UserEntity.fromFirestore(DocumentSnapshot snapshot) {
-    if (snapshot == null) return null;
-    var map = snapshot.data();
+    final Map<String, dynamic> map = snapshot.data() as Map<String, dynamic>;
 
     return UserEntity(
-      emailAddress: map['emailAddress'],
-      lastName: map['lastName'],
-      firstName: map['firstName'],
+      emailAddress: map['emailAddress'] as String,
+      lastName: map['lastName'] as String,
+      firstName: map['firstName'] as String,
       snapshot: snapshot,
       reference: snapshot.reference,
       userID: snapshot.id,
@@ -32,36 +27,74 @@ class UserEntity {
   }
 
   factory UserEntity.fromMap(Map<String, dynamic> map) {
-    if (map == null) return null;
-
     return UserEntity(
-      emailAddress: map['emailAddress'],
-      lastName: map['lastName'],
-      firstName: map['firstName'],
+      userID: map['userID'] as String,
+      emailAddress: map['emailAddress'] as String,
+      lastName: map['lastName'] as String,
+      firstName: map['firstName'] as String,
+      snapshot: map['snapshot'] as DocumentSnapshot,
+      reference: map['reference'] as DocumentReference,
     );
   }
 
-  Map<String, dynamic> toMap() => {
+  factory UserEntity.fromJson(String json) {
+    return UserEntity.fromMap(jsonDecode(json) as Map<String, dynamic>);
+  }
+
+  final String emailAddress;
+  final String lastName;
+  final String firstName;
+  final DocumentSnapshot snapshot;
+  final DocumentReference reference;
+
+  /// To use Firebase Auth UID as key in `users` collection
+  ///
+  /// Reuse documentID
+  /// Should not have any nulls as we query this directly from Firestore
+  /// and sign up internally
+  ///
+  final String? userID;
+
+  Map<String, dynamic> toMap() => <String, String?>{
+        'userID': userID,
         'emailAddress': emailAddress,
         'lastName': lastName,
         'firstName': firstName,
       };
 
   UserEntity copyWith({
-    String emailAddress,
-    String lastName,
-    String firstName,
+    String? emailAddress,
+    String? lastName,
+    String? firstName,
+
+    /// Copy over fields while keeping the same ID snapshot references?
+    ///
+    String? userID,
+    DocumentSnapshot? snapshot,
+    DocumentReference? reference,
   }) {
     return UserEntity(
+      userID: userID ?? this.userID,
       emailAddress: emailAddress ?? this.emailAddress,
       lastName: lastName ?? this.lastName,
       firstName: firstName ?? this.firstName,
+      snapshot: snapshot ?? this.snapshot,
+      reference: reference ?? this.reference,
     );
+  }
+
+  /// The data parts only?
+  Map<String, Object?> toDocument() {
+    return {
+      'emailAddress': emailAddress,
+      'lastName': lastName,
+      'firstName': firstName,
+    };
   }
 
   @override
   String toString() {
-    return '${emailAddress.toString()}, ${lastName.toString()}, ${firstName.toString()}, ';
+    return '${userID.toString()}, ${emailAddress.toString()}, ${lastName.toString()}, ${firstName.toString()}, ';
   }
 
   @override
