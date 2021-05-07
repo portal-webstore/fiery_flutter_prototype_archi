60db945307653d65a0076e8e38ba3e2b7a26539d
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-07 12:02:46 +1000

    refactor(order_repository,order_model): update fromMap conversion for strong nullsafety
    Subsequently .fromJson (reuses fromMap). Note some strange code around the data assumptions

--- packages/order_repository/lib/src/models/dartgen/order_model.dart
+++ packages/order_repository/lib/src/models/dartgen/order_model.dart
@@ -1,8 +1,8 @@
-import 'dart:convert';
+import 'dart:convert' show json, jsonDecode;
 
 import 'package:collection/collection.dart';
 
-import 'patient_treatment_product_item.dart';
+import 'patient_treatment_product_item.dart' show PatientTreatmentProductItem;
 
 class Order {
   final String orderID;
@@ -12,7 +12,8 @@ class Order {
   final String requiredByDeliveryDate;
   final String comments;
   final bool isDraft;
-  final List<PatientTreatmentProductItem> patientTreatmentProductItems;
+  final List<PatientTreatmentProductItem>? patientTreatmentProductItems;
+
   const Order({
     required this.orderID,
     required this.orderReference,
@@ -39,15 +40,17 @@ class Order {
       orderReference: orderReference ?? this.orderReference,
       createdAt: createdAt ?? this.createdAt,
       updatedAt: updatedAt ?? this.updatedAt,
-      requiredByDeliveryDate: requiredByDeliveryDate ?? this.requiredByDeliveryDate,
+      requiredByDeliveryDate:
+          requiredByDeliveryDate ?? this.requiredByDeliveryDate,
       comments: comments ?? this.comments,
       isDraft: isDraft ?? this.isDraft,
-      patientTreatmentProductItems: patientTreatmentProductItems ?? this.patientTreatmentProductItems,
+      patientTreatmentProductItems:
+          patientTreatmentProductItems ?? this.patientTreatmentProductItems,
     );
   }
 
   Map<String, dynamic> toMap() {
-    return {
+    return <String, Object?>{
       'orderID': orderID,
       'orderReference': orderReference,
       'createdAt': createdAt,
@@ -55,26 +58,36 @@ class Order {
       'requiredByDeliveryDate': requiredByDeliveryDate,
       'comments': comments,
       'isDraft': isDraft,
-      'patientTreatmentProductItems': patientTreatmentProductItems?.map((x) => x.toMap())?.toList(),
+      'patientTreatmentProductItems':
+          patientTreatmentProductItems?.map((x) => x.toMap()).toList(),
     };
   }
 
   factory Order.fromMap(Map<String, dynamic> map) {
     return Order(
-      orderID: map['orderID'],
-      orderReference: map['orderReference'],
-      createdAt: map['createdAt']?.toInt(),
-      updatedAt: map['updatedAt']?.toInt(),
-      requiredByDeliveryDate: map['requiredByDeliveryDate'],
-      comments: map['comments'],
-      isDraft: map['isDraft'],
-      patientTreatmentProductItems: List<PatientTreatmentProductItem>.from(map['patientTreatmentProductItems']?.map((x) => PatientTreatmentProductItem.fromMap(x))),
+      orderID: map['orderID'] as String,
+      orderReference: map['orderReference'] as String,
+      createdAt: (map['createdAt'] as num?)?.toInt() ?? 0,
+      updatedAt: (map['updatedAt'] as num?)?.toInt() ?? 0,
+      requiredByDeliveryDate: map['requiredByDeliveryDate'] as String,
+      comments: map['comments'] as String,
+      isDraft: map['isDraft'] as bool,
+      patientTreatmentProductItems: List<PatientTreatmentProductItem>.from(
+        (map['patientTreatmentProductItems'] as Iterable<dynamic>? ??
+                <dynamic>[])
+            .map<PatientTreatmentProductItem>(
+          (dynamic x) => PatientTreatmentProductItem.fromMap(
+            x as Map<String, dynamic>,
+          ),
+        ),
+      ),
     );
   }
 
   String toJson() => json.encode(toMap());
 
-  factory Order.fromJson(String source) => Order.fromMap(json.decode(source));
+  factory Order.fromJson(String source) =>
+      Order.fromMap(jsonDecode(source) as Map<String, dynamic>);
 
   @override
   String toString() {
@@ -85,27 +98,30 @@ class Order {
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
     final listEquals = const DeepCollectionEquality().equals;
-  
+
     return other is Order &&
-      other.orderID == orderID &&
-      other.orderReference == orderReference &&
-      other.createdAt == createdAt &&
-      other.updatedAt == updatedAt &&
-      other.requiredByDeliveryDate == requiredByDeliveryDate &&
-      other.comments == comments &&
-      other.isDraft == isDraft &&
-      listEquals(other.patientTreatmentProductItems, patientTreatmentProductItems);
+        other.orderID == orderID &&
+        other.orderReference == orderReference &&
+        other.createdAt == createdAt &&
+        other.updatedAt == updatedAt &&
+        other.requiredByDeliveryDate == requiredByDeliveryDate &&
+        other.comments == comments &&
+        other.isDraft == isDraft &&
+        listEquals(
+          other.patientTreatmentProductItems,
+          patientTreatmentProductItems,
+        );
   }
 
   @override
   int get hashCode {
     return orderID.hashCode ^
-      orderReference.hashCode ^
-      createdAt.hashCode ^
-      updatedAt.hashCode ^
-      requiredByDeliveryDate.hashCode ^
-      comments.hashCode ^
-      isDraft.hashCode ^
-      patientTreatmentProductItems.hashCode;
+        orderReference.hashCode ^
+        createdAt.hashCode ^
+        updatedAt.hashCode ^
+        requiredByDeliveryDate.hashCode ^
+        comments.hashCode ^
+        isDraft.hashCode ^
+        patientTreatmentProductItems.hashCode;
   }
 }
