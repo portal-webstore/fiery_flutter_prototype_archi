dc1f273658e4760e113e33a18de56b378bd9681c
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-07 15:49:51 +1000

    refactor(test): scaffold folders in reflection of lib/ for consistency
    bloc folder, widget arbitrary test adjustment

--- test/widgets/widget_test.dart
+++ test/widgets/widget_test.dart
@@ -5,26 +5,19 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:fiery_flutter_prototype_archi/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fiery_flutter_prototype_archi/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Menu is clickable', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Find a menu on home app screen.
+    expect(find.byIcon(Icons.menu), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byKey(const Key('__menu_button__')));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
 }
