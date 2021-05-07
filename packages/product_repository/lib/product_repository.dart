619c906698b38c6a68bd9911da0443ac53e811ca
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-07 13:04:17 +1000

    build(product_repository): add base product_repository package
    .. Can probably convert the generated model out over into the individual repositories and expose
    here. (Tested data class codegen from seed_orders.json)

---
+++ packages/product_repository/lib/product_repository.dart
@@ -0,0 +1,7 @@
+library product_repository;
+
+/// {@template product_repository}
+/// Repository to manage product info.
+/// May map from clinic preferences
+/// @endtemplate}
+class ProductRepository {}
