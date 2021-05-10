7de364b199db359a46d53499da0510ea2921e808
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-10 12:03:24 +1000

    chore(shared/routes): add base routes
    ONCWEB-19 project scaffolding

---
+++ lib/shared/routes/routes.dart
@@ -0,0 +1,12 @@
+/// {@template routes}
+/// Static routes paths.
+///
+///
+/// {@endtemplate}
+/// See {@macro route_generator}
+class Routes {
+  /// "Static" class for namespacing
+  const Routes._();
+
+  static const String home = '/';
+}
