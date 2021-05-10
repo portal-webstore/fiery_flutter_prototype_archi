854e0eca19ffe28929f930ff2e3b3debb962c5da
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-11 09:26:13 +1000

    build(form_input_helpers): add form input helpers dep
    Formatters, validators, general utility
    
    Verbose date validator utility ported. Ready for refactoring trade-offs.
    Scaffold the tests/ project structure

---
+++ packages/form_input_helpers/lib/form_input_helpers.dart
@@ -0,0 +1,1 @@
+library form_input_helpers;
---
+++ packages/form_input_helpers/lib/src/shared/text/formatters/uppercase_text_formatter.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/services.dart'
+    show TextEditingValue, TextInputFormatter;
+
+class UpperCaseTextFormatter extends TextInputFormatter {
+  @override
+  TextEditingValue formatEditUpdate(
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    return TextEditingValue(
+      text: newValue.text.toUpperCase(),
+      selection: newValue.selection,
+    );
+  }
+}
