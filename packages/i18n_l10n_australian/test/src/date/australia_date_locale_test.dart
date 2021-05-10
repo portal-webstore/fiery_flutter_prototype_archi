e54999f7be4a3a98832927061be643ccf09028ff
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-11 09:24:06 +1000

    test(i18n_l10n_australian): scaffold test structure placeholders

---
+++ packages/i18n_l10n_australian/test/src/date/australia_date_locale_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Australian date locale', () {
+    test('formats full date display text dd/MM/yyyy', () {
+      expect(true, true);
+    });
+    test('formats ISO 8601 universal UTC timestamp', () {
+      expect(true, true);
+    });
+  });
+}
