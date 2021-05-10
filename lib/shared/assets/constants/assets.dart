009e631e4ca520eb8e1758b34219a8fa966175f1
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-10 13:41:09 +1000

    feat(shared/assets): add logo image file path
    declared in pubspec package

---
+++ lib/shared/assets/constants/assets.dart
@@ -0,0 +1,11 @@
+/// {@template assets}
+/// Provide access paths to packaged assets
+/// @{endtemplate}
+class Assets {
+  /// Namespacing only.
+  Assets._();
+
+  /// Logo
+  /// Should be resolution-aware
+  static const String appLogo = 'assets/images/icons/logo.png';
+}
