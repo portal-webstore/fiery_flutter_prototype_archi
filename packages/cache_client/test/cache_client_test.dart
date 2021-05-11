ea7650b2909f270a764e0ed65a199d8f5825b399
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-11 14:29:30 +1000

    test(cache_client): add simple key-value read/write confirmation test
    Also check overwrite recency as confirmed inherent test-documented
    behaviour

--- packages/cache_client/test/cache_client_test.dart
+++ packages/cache_client/test/cache_client_test.dart
@@ -1,12 +1,57 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:cache_client/cache_client.dart' show CacheClient;
+import 'package:test/test.dart' show expect, group, test;
 
 void main() {
   group('caches values', () {
+    final CacheClient cacheClient = CacheClient();
+
+    test('read null no value', () {
+      final String? valueRead = cacheClient.read<String>(key: 'nothinghere');
+      expect(valueRead, null);
+    });
     test('read/write key', () {
-      expect(true, true);
+      /// Assuming no practical limitations on character set
+
+      const String keyWritten = 'Hello!';
+      const String valueWritten = 'ho there';
+      cacheClient.write(
+        key: keyWritten,
+        value: valueWritten,
+      );
+
+      final String? valueRead = cacheClient.read<String>(key: keyWritten);
+
+      expect(valueRead, valueWritten);
+    });
+    test('overwrite key with most recent value', () {
+      /// Assuming no practical limitations on character set
+
+      const String keyWritten = 'Hello!';
+      const String valueWritten = 'ho there';
+      const String valueOverwritten = 'Goodbye';
+      const String valueOverwrittenOverwritten = 'See you later';
+
+      cacheClient.write(key: keyWritten, value: valueWritten);
+      cacheClient.write(key: keyWritten, value: valueOverwritten);
+
+      final String? valueRead = cacheClient.read<String>(key: keyWritten);
+
+      expect(valueRead, valueOverwritten);
+
+      cacheClient.write(key: keyWritten, value: valueOverwritten);
+      cacheClient.write(key: keyWritten, value: keyWritten);
+      cacheClient.write(key: keyWritten, value: valueWritten);
+      cacheClient.write(key: keyWritten, value: valueOverwrittenOverwritten);
+
+      final String? valueReadAgainOnSameKey = cacheClient.read<String>(
+        key: keyWritten,
+      );
+
+      expect(valueReadAgainOnSameKey, valueOverwrittenOverwritten);
     });
     test('web memory limit', () {
       /// Find the load test point
+      /// ? Platform/runtime dependent
       expect(true, true);
     });
   });
