b65108908ccf85126cb8fd11145aa357850ff8bc
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-11 14:19:29 +1000

    build(cache_client): init base package template

---
+++ packages/cache_client/lib/cache_client.dart
@@ -0,0 +1,2 @@
+library cache_client;
+
---
+++ packages/cache_client/test/cache_client_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('caches values', () {
+    test('read/write key', () {
+      expect(true, true);
+    });
+    test('web memory limit', () {
+      /// Find the load test point
+      expect(true, true);
+    });
+  });
+}
