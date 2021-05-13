0999edcc69ac9ff91960b59a4d7a35f40711c152
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-14 07:35:39 +1000

    refactor(home_screen): replace unsafe null-unwrap! with coerced home title
    Preserve static runtime safety

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -23,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title!),
+        title: Text(widget.title ?? 'Home'),
       ),
       // - TODO: Add drawer for side menu navigation fallback
 
