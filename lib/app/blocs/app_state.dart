e4ae1fece4c6a2821df8d37da8f7349eaf47918d
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-14 09:02:35 +1000

    refactor(app_state): explain doc inconsistency between enums vs sealed class representation
    See commented hyperlinks

--- lib/app/blocs/app_state.dart
+++ lib/app/blocs/app_state.dart
@@ -11,7 +11,10 @@ class AppState extends Equatable {
     this.user = User.empty,
   });
 
-  /// Expoes two named constructors to make it "easier to work with"
+  /// - TODO: Refactor this back out after getting working login states
+  /// Refactor back out into separate sealed states classes for cleaner expand
+  ///
+  /// Exposes two named constructors to make it "easier to work with"
   /// is this laziness?
   ///
   /// The other example in the **same documentation** follows the convention of
@@ -20,9 +23,15 @@ class AppState extends Equatable {
   /// - Authenticated()
   /// - Unauthenticated()
   const AppState.authenticated(User user)
-      : this._(status: AppStatus.authenticated, user: user);
+      : this._(
+          status: AppStatus.authenticated,
+          user: user,
+        );
 
-  const AppState.unauthenticated() : this._(status: AppStatus.unauthenticated);
+  const AppState.unauthenticated()
+      : this._(
+          status: AppStatus.unauthenticated,
+        );
 
   final AppStatus status;
   final User user;
@@ -30,3 +39,22 @@ class AppState extends Equatable {
   @override
   List<Object> get props => [status, user];
 }
+
+/// It's an odd choice to pick and choose where the documented examples feel
+/// like taking the quick way out compared to supposed scalable architecture way
+///
+///
+/// Should we not prefer the sealed classes `is` pattern matching paradigm
+/// for BLoC?
+///
+/// Rather than limiting the scalability to enum...
+/// As long as we do not have duplicated conditional logic.
+///
+/// `is` pattern matching is nice
+///
+/// See another example
+/// https://github.com/fiery-flutter/currency-rates/commit/0e5af287375f510e1818ff864e7c27778a95fe45/lib/src/feature/rates/view/bloc/currency_rate_state.dart
+/// 
+/// https://stackoverflow.com/questions/54317000/are-there-any-sealed-classes-alternatives-in-dart-2-0
+/// 
+/// https://github.com/dart-lang/language/issues/349#issuecomment-388912316
