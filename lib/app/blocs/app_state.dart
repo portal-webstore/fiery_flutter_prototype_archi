d0e0edff3b9ef5d216e60c74331d9e159a249566
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-14 08:48:55 +1000

    docs(app_state): annotate more inconsistency within the same docs
    scaleable/maintainable without standardisation??!!

--- lib/app/blocs/app_state.dart
+++ lib/app/blocs/app_state.dart
@@ -11,6 +11,14 @@ class AppState extends Equatable {
     this.user = User.empty,
   });
 
+  /// Expoes two named constructors to make it "easier to work with"
+  /// is this laziness?
+  ///
+  /// The other example in the **same documentation** follows the convention of
+  /// distinct state classes:
+  /// - Uninitialised()
+  /// - Authenticated()
+  /// - Unauthenticated()
   const AppState.authenticated(User user)
       : this._(status: AppStatus.authenticated, user: user);
 
