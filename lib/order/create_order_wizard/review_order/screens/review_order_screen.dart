cc0837dd9984fd654424748de293dc617c44b24b
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-14 15:23:52 +1000

    chore(lib,clinic,order): scaffolding project structure for orders
    Revert or rebase pick apart to only add the files as they are needed per
    their feature folders for less noisy commits

---
+++ lib/order/create_order_wizard/review_order/screens/review_order_screen.dart
@@ -0,0 +1,5 @@
+/// Button decision model based on priority
+/// 
+/// 1. Add another drug to the same patient
+/// 2. Add another patient to the same order
+/// 3. Review / submit (or save draft for resumption later)
---
+++ lib/order/views/screens/add_treatment_product_item_screen.dart
@@ -0,0 +1,5 @@
+/// Add treatment product item for the intended patient
+/// More than one may be added
+/// i.e. close each one on saving item
+/// Minimise editability depending on drafting complexity.
+/// 
