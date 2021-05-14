f2092a6c6eb36bb5276cbbfec50eeef215d7aa79
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-14 10:21:32 +1000

    feat(webstore_app): add localised themes material app before refactor
    Moving this out to follow weird sample convention

--- lib/app/views/webstore_app.dart
+++ lib/app/views/webstore_app.dart
@@ -1,7 +1,12 @@
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:colour_style_guide/colour_style_guide.dart';
 import 'package:fiery_flutter_prototype_archi/home/screens/home_screen.dart';
 import 'package:fiery_flutter_prototype_archi/shared/routes/route_generator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart'
+    show GlobalMaterialLocalizations, GlobalWidgetsLocalizations;
+import 'package:i18n_l10n_australian/i18n_l10n_australian.dart'
+    show AustralianAppLocalisationDelegate;
 
 class WebstoreApp extends StatelessWidget {
   const WebstoreApp({
@@ -15,22 +20,23 @@ class WebstoreApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //? add call in highest initState/widget?
     // initializeDateFormatting(en_AU);
+    // Or rely in date locales already plugged in, delegates.
 
     return MaterialApp(
       title: 'Webstore portal home',
-      // - TODO: Use our own custom localisation delegate
-      // localizationsDelegates: const [
-      //   // AustralianLocalisationDelegate(),
-      //   // GlobalMaterialLocalizations.delegate,
-      //   // GlobalWidgetsLocalizations.delegate,
-      // ],
+      localizationsDelegates: const [
+        AustralianAppLocalisationDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       supportedLocales: const [
         Locale('en', 'AU'), // English (Australia)
         Locale('en', 'US'), // English (United States) for safe defaults
       ],
       // - TODO: Use our own custom theme package
-      theme: ThemeData(primaryColor: Colors.blueGrey),
+      theme: ColouredTheme.data,
       home: const HomeScreen(
         title: 'Webstore portal home',
       ),
