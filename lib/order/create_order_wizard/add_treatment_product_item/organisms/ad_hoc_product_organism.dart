7e5213069186be5dfd78150c29c04e303bc5f04c
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-17 10:34:35 +1000

    chore(create_order_wizard): add base atomic files for project scaffolding
    Design-thinking - non feature non vertical slice

---
+++ lib/order/create_order_wizard/add_treatment_product_item/organisms/ad_hoc_product_organism.dart
@@ -0,0 +1,4 @@
+/// Higher complexity as it meshes up the model to have dose co-located
+/// with the additional custom free-text dropdown fields
+/// rather than pre-selecting the entire product
+///
