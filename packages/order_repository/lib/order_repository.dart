4ed19551d9679461d3fc7cc255a451b3820806de
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-18 15:07:27 +1000

    build(order_repository): export FirebaseOrderRepository for use in screen logic interface prototype

--- packages/order_repository/lib/order_repository.dart
+++ packages/order_repository/lib/order_repository.dart
@@ -1,9 +1,10 @@
 library order_repository;
 
+export 'src/firebase_order_repository.dart' show FirebaseOrderRepository;
+
 /// Base order models
 export 'src/models/models.dart' show Order, OrderEntity;
-export 'src/order_repository.dart'
-    show FirebaseOrderRepository, OrderRepository;
+export 'src/order_repository.dart' show OrderRepository;
 
 /// Treatment specific patient treatment item models
 export 'src/treatment/treatment.dart'
