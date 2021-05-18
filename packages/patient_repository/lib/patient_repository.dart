69b6af0297e13f199cd36a638129338458963395
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-18 11:01:10 +1000

    chore(patient_repository): remove unused export display name, coalesced under the single extension
    This function symbol no longer exists after the refactor to include i18n_l10n_localisation.
    Extension PatientBirthDateAustralian contains the getDisplay viewmodel from patient as it depends on
    date formatting as well. Could alternatively wrap it all under the single [Patient] model class...

--- packages/patient_repository/lib/patient_repository.dart
+++ packages/patient_repository/lib/patient_repository.dart
@@ -1,5 +1,4 @@
 library patient_repository;
 
-export 'src/models/models.dart'
-    show Patient, PatientBirthDateAustralian, getNameTextFromPatient;
+export 'src/models/models.dart' show Patient, PatientBirthDateAustralian;
 export 'src/patient_repository.dart' show PatientRepository;
