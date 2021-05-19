6fb5824822192c10140fd60fbe720a1bdf329fbd
S <30815731+slimlime@users.noreply.github.com>
2021-05-19 11:35:02 +1000

    feat(order_repository,order_entity): parse dynamic list patientTreatmentProductItems from firestore
    Updated to completely allow querying the orders collection (as long as the previously submitted
    orders conform to our expected data, otherwise things may explode, untested)
    
    BREAKING CHANGE: Fixes the errors in parsing due to Flutterfire runtime List map dynamic preference

--- packages/order_repository/lib/src/models/order_entity.dart
+++ packages/order_repository/lib/src/models/order_entity.dart
@@ -20,6 +20,26 @@ class OrderEntity {
   factory OrderEntity.fromFirestore(DocumentSnapshot snapshot) {
     final Map<String, Object?> map = snapshot.data() as Map<String, Object?>;
 
+    if (map['patientTreatmentProductItems'] == null) {
+      throw FormatException(
+        'Expected patient treatment product items maps in a list. '
+        'Please check data store or conversion adaptors and try again',
+      );
+    }
+
+    /// Dart runtime type as List<dynamic> ...
+    /// Bugs out if we cast List<Map<String, Object?>>
+    /// In Dart, the element type of a list is provided when the list is created,
+    /// See explanation https://stackoverflow.com/a/55577750
+    /// The list is set by the json parser at time of parse (API upstream to us)
+    final List<dynamic> patientTreatmentProductMaps =
+        map['patientTreatmentProductItems'] as List<dynamic>;
+
+    final List<PatientTreatmentProductItem> patientTreatmentProductItems =
+        getListPatientTreatmentProductsFromFirestoreJsonMaps(
+      patientTreatmentProductMaps,
+    );
+
     return OrderEntity(
       orderID: map['orderID'] != null ? map['orderID'] as String : null,
       orderReference: map['orderReference'] != null
@@ -32,11 +52,7 @@ class OrderEntity {
           : null,
       comments: map['comments'] != null ? map['comments'] as String : null,
       isDraft: map['isDraft'] != null ? map['isDraft'] as bool : null,
-      patientTreatmentProductItems: map['patientTreatmentProductItems'] != null
-          ? getListPatientTreatmentProductsFromMap(
-              map['patientTreatmentProductItems'],
-            )
-          : null,
+      patientTreatmentProductItems: patientTreatmentProductItems,
       snapshot: snapshot,
       reference: snapshot.reference,
       documentID: snapshot.id,
@@ -47,19 +63,29 @@ class OrderEntity {
   factory OrderEntity.fromSnapshot(DocumentSnapshot snapshot) =>
       OrderEntity.fromFirestore(snapshot);
 
+  /// Note that (flutter)firestore document snapshot data maps list as <dynamic>
+  /// Runtime type List<dynamic> is kept part of the list value
+  /// Even though we only want it for readonly purposes
   static List<PatientTreatmentProductItem>
-      getListPatientTreatmentProductsFromMap(
-    dynamic patientTreatmentProductsMappedFromSnapshot,
+      getListPatientTreatmentProductsFromFirestoreJsonMaps(
+    List<dynamic>? patientTreatmentProductsMappedFromSnapshot,
   ) {
-    return List<PatientTreatmentProductItem>.from(
-      (patientTreatmentProductsMappedFromSnapshot as Iterable<dynamic>? ??
-              <dynamic>[])
-          .map<PatientTreatmentProductItem>(
-        (dynamic x) => PatientTreatmentProductItem.fromMap(
-          x as Map<String, dynamic>,
-        ),
-      ),
-    );
+    final List<dynamic> blankedList =
+        patientTreatmentProductsMappedFromSnapshot ?? <Map<String, Object?>>[];
+
+    final List<PatientTreatmentProductItem>
+        objectifiedPatientTreatmentProducts =
+        blankedList.map<PatientTreatmentProductItem>(
+      (
+        dynamic productItemMap,
+      ) {
+        // We finally coerce the map type information here from dynamic
+        return PatientTreatmentProductItem.fromMap(
+            productItemMap as Map<String, Object?>);
+      },
+    ).toList();
+
+    return objectifiedPatientTreatmentProducts;
   }
 
   factory OrderEntity.fromMap(Map<String, dynamic> map) {
