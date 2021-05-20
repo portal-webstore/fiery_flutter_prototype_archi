b8bca8ffef2f0f95b60f8a53b06d021d68b4c0d6
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-20 14:33:04 +1000

    refactor(order_repository): rearrange CRUD api for consistency
    Arbitrary default number of documents to limit query to minimise magic numbers

--- packages/order_repository/lib/src/order_repository.dart
+++ packages/order_repository/lib/src/order_repository.dart
@@ -1,23 +1,33 @@
-import 'package:order_repository/order_repository.dart' show Order;
+import 'package:order_repository/order_repository.dart'
+    show Order, PatientTreatmentProductItem;
 
 /// {@template order_repository}
 /// Repository which retrieves static order list
 /// {@endtemplate}
 abstract class OrderRepository {
+  static const int arbitraryDocQueryCountLimit = 6;
+
   /// Create
   Future<void> addNewOrder(Order order);
 
-  /// Delete
-  Future<void> deleteOrder(Order order);
-
-  Future<Order?> getOrder(Order order);
+  /// **Read**
+  /// Get a single order
+  Future<Order?> getOrder(String orderID);
 
   /// Read
   /// We have dynamic orders
   ///
   /// Potentially limited retention to four weeks ~ 31 days
-  Stream<List<Order>> orders({int limitMostRecentCount = 6});
+  ///
+  /// Stream of orders for ongoing reads listening?
+  ///
+  Stream<List<Order>> orders({
+    int limitMostRecentCount = arbitraryDocQueryCountLimit,
+  });
 
   /// Update
   Future<void> updateOrder(Order order);
+
+  /// Delete
+  Future<void> deleteOrder(Order order);
 }
