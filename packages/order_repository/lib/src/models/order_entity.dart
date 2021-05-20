0945ff004fc9bf868e28879748071a58795dfac6
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-20 13:11:11 +1000

    refactor(order_repository,entity): remove unused import
    Likely an IDE auto-suggest remnant FromFirestore when intending to import an Order.fromFirestore
    converter function instead

--- packages/order_repository/lib/src/models/order_entity.dart
+++ packages/order_repository/lib/src/models/order_entity.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart'
-    show DocumentReference, DocumentSnapshot, FromFirestore;
+    show DocumentReference, DocumentSnapshot;
 import 'package:order_repository/src/treatment/models/models.dart';
 
 class OrderEntity {
