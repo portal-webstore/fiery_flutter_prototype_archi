08dd6c87aba098b7891dd100709b53d807a54ef7
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-21 11:13:51 +1000

    feat(dropdown,models,seeds): add seed data dropdown assistive free text for ad hoc creation
    for semi-structured ad hoc product creation composed of admin route, container, diluent,
    dongle(attachment), drug(s), dose

---
+++ packages/product_repository/lib/src/dropdown/seeds/diluent_dropdowns.dart
@@ -0,0 +1,15 @@
+import 'package:product_repository/src/dropdown/models/diluent_dropdown_model.dart'
+    show DiluentDropdown;
+
+/// Diluents for dropdown-assistive free text
+const diluentDropdowns = [
+  DiluentDropdown(
+    diluentName: 'Dext 5',
+  ),
+  DiluentDropdown(
+    diluentName: 'N/S',
+  ),
+  DiluentDropdown(
+    diluentName: 'WFI',
+  ),
+];
---
+++ packages/product_repository/lib/src/dropdown/seeds/dongle_dropdowns.dart
@@ -0,0 +1,27 @@
+import 'package:product_repository/src/dropdown/models/dongle_dropdown_model.dart'
+    show DongleDropdown;
+
+/// Dongles for dropdown-assistive free text
+const List<DongleDropdown> dongleDropdowns = [
+  DongleDropdown(
+    dongleName: 'CH12',
+  ),
+  DongleDropdown(
+    dongleName: 'Cyto-Set',
+  ),
+  DongleDropdown(
+    dongleName: 'CS10',
+  ),
+  DongleDropdown(
+    dongleName: 'Spiros',
+  ),
+  DongleDropdown(
+    dongleName: 'PhaSeal N35',
+  ),
+  DongleDropdown(
+    dongleName: 'Extension Set',
+  ),
+  DongleDropdown(
+    dongleName: 'None',
+  ),
+];
