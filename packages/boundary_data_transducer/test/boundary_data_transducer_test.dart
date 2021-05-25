7e0eecb6b113d978a9b9521e99bb8cc869d28308
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-25 13:44:09 +1000

    test(boundary_data_transducer): check success/faliure criteria, graceful mix success conversions
    Check for happiest path valid conversions, check invalid conversions all emptied, check mix retained
    valid conversions only. TDD we found that the Exception handler catch did not appropriately handle
    the case of missing keys with our example class/constructor. This was due to the map field not
    checking null (and we do not want it to silently create the object when not all fields are valid,
    anyway). Expected an exception to be raised; however, Dart throws a _CastError that we now handle
    with TypeError catching.
    
    The alternative would be to expect the developer to balloon out every
    class parsing boilerplate (manual handcoding at the moment as template
    are not all updated!) maybe double the number of lines to read/maintain
    depending on the types of nested objects and the onus of adding null checking and argument exception throws to every map[''] field subscript access operation before use...
    
    Potential use case for code generation, json serialisation possibility?
    Currently using VS Code data class generator extensions + handcoded
    amendments to linting and convention

--- packages/boundary_data_transducer/test/boundary_data_transducer_test.dart
+++ packages/boundary_data_transducer/test/boundary_data_transducer_test.dart
@@ -1,7 +1,104 @@
+import 'package:boundary_data_transducer/boundary_data_transducer.dart'
+    show constructValidObjectsFromData;
 import 'package:test/test.dart';
 
+import 'mocks/sample_construction_parsed_object_model.dart'
+    show SampleConstructionParsedObject;
+
 void main() {
-  test('test', () {
+  setUp(() {
+    print('setup main');
     return;
   });
+  group('parse', () {
+    const SampleConstructionParsedObject expectedConstructedValid =
+        SampleConstructionParsedObject(
+      test: 'test',
+    );
+
+    const SampleConstructionParsedObject
+        expectedConstructedValidExtraFieldsIgnored = expectedConstructedValid;
+
+    const Map<String, Object?> sampleMapValid = {
+      'test': 'test',
+    };
+    const Map<String, Object?> sampleMapInvalidKey = {
+      'invalidkeytest': -1,
+    };
+    const Map<String, Object?> sampleMapValidExtraFieldsIgnored = {
+      'test': 'test',
+      'extraField': <String, String>{
+        'whyExtraFieldHere': '?to test buggy code',
+      }
+    };
+    const Map<String, Object?> sampleMapInvalidCorrectKeyIncorrectType = {
+      'test': 9001,
+    };
+
+    const List<Map<String, Object?>> sampleMaps = [
+      sampleMapValid,
+      sampleMapInvalidKey,
+      sampleMapValidExtraFieldsIgnored,
+      sampleMapInvalidCorrectKeyIncorrectType,
+    ];
+
+    setUp(() {
+      print('setup parse group');
+      return;
+    });
+
+    test('valid construction', () {
+      final objects = constructValidObjectsFromData(
+        [sampleMapValid],
+        _mapConstructor,
+      );
+
+      final List<SampleConstructionParsedObject> expectedObjects = [
+        expectedConstructedValid,
+      ];
+      expect(objects, expectedObjects);
+    });
+
+    test('empty list on all invalid construction', () {
+      final objects = constructValidObjectsFromData(
+        [
+          sampleMapInvalidKey,
+          sampleMapInvalidCorrectKeyIncorrectType,
+        ],
+        _mapConstructor,
+      );
+
+      final List<SampleConstructionParsedObject> expectedObjectsEmpty = [];
+      expect(objects, expectedObjectsEmpty);
+      expect(objects.isEmpty, true);
+    });
+
+    test('retain only valid non-null map data', () {
+      final objects = constructValidObjectsFromData(
+        [
+          sampleMapValid, // ✅ Base success case
+          sampleMapInvalidKey, // ❌ Completely fail
+          sampleMapValidExtraFieldsIgnored, // ✅ Should succeed
+          sampleMapInvalidCorrectKeyIncorrectType, // ❌ Should fail with TypeImplError
+        ],
+        _mapConstructor,
+      );
+
+      // Expect only two here
+      final List<SampleConstructionParsedObject> expectedObjects = [
+        expectedConstructedValid,
+        expectedConstructedValidExtraFieldsIgnored,
+      ];
+
+      expect(objects, expectedObjects);
+      // Redundant expectation for clarity magic number baked test
+      expect(objects.length, 2);
+    });
+  });
+}
+
+SampleConstructionParsedObject _mapConstructor(dynamic map) {
+  return SampleConstructionParsedObject.fromMap(
+    map as Map<String, dynamic>,
+  );
 }
