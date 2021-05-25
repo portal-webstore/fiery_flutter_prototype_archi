c1bd6f474474fa8280e5eda0906abb6a239b79cd
webs <82124333+webstoreportal@users.noreply.github.com>
2021-05-25 13:36:19 +1000

    feat(boundary_data_transducer,try_parse): handle null field access data map transform (gracefully)
    Null TypeError is thrown when accessing a non-existent (null) field from the dynamic map.
    We need to catch this to allow us to ignore the invalid construction to
    afford the continuity of the app
    i.e. Multiple orders or items, we would rather kill only one order
    object rather than completely break the app.
    
    Internally catch with contextual explanation to afford developer intended usage. Knowingly going
    against grain of some lint rules and conventions for this use case at the data conversion boundary.
    We catch type error to be able to ignore (fail early!) parts of a mapped array during construction,
    based on data received by an external provider. Due to the potential impedance slowly shifting
    dimensions, we want to enforce the developer to make sure their frontend model is complete before
    passing into our domain. To minimise further inconsistency.

--- packages/boundary_data_transducer/lib/src/utils/try_parse_data_transform.dart
+++ packages/boundary_data_transducer/lib/src/utils/try_parse_data_transform.dart
@@ -16,9 +16,32 @@ T? tryParseDataTransform<T>(
     return parsedObject;
   } on Exception catch (exception) {
     print(
-      'Unable to construct/parse object from given data: '
+      'Exception: Unable to construct/parse object from given data: '
       '${exception.toString()}',
     );
+  } on TypeError catch (error) {
+    // Big bad!
+    // Means developer did not check null accessors when using map[''] subscript
+    /// _errorMsg:"type 'Null' is not a subtype of type 'String' in type cast"
+    print(
+      'TYPE ERROR: Unable to construct/parse object from given data: '
+      'This is likely due to unsafe null access from map. \n'
+      'Please make sure to check for null field before attempting to access '
+      "a map subscript key. i.e. map['key'] != null ? ?? throw ArgumentError ??"
+      '\n'
+      '${error.toString()}',
+    );
+
+    /// We handle this internal to the library for usefulness
+    /// Otherwise we probably should not catch errors per common practice
+    /// http://www.codinghorror.com/blog/archives/000358.html
+    /// https://stackoverflow.com/a/467628
+    /// Exceptions should be exceptional
+    /// Performance tax?
+    ///
+    /// Safety importance.
+    /// In case developer forgets to unit test their constructor conversions
+    /// separately.
   }
 
   return null;
